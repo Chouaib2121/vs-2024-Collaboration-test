44365f056489f88ade16af9358cb88917c9bf8f7
Chouaib Aifour <chouaibaifour1@gmail.com>
2024-01-19 09:25:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace vs_2024_Collaboration_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
